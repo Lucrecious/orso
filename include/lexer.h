55ccd264a739f79933e248cfe33ec9c28170d967
Lucrecious <luca.duran@hotmail.com>
2022-10-21 09:07:25 -0400

    Write lexer

--- include/lexer.h
+++ include/lexer.h
@@ -5,27 +5,40 @@
 #include "error_codes.h"
 
 typedef enum TokenType {
-    TokenType_INVALID,
-    TokenType_IDENTIFIER,
-    TokenType_COLIN,
-    TokenType_EQUALS,
-    TokenType_INTEGER,
-    TokenType_EOF,
-    TokenType_SIZE,
+    // single character
+    TOKEN_PARENTHESIS_OPEN, TOKEN_PARENTHESIS_CLOSE,
+    TOKEN_BRACE_OPEN, TOKEN_BRACE_CLOSE,
+    TOKEN_BRACKET_OPEN, TOKEN_BRACKET_CLOSE,
+    TOKEN_COMMA, TOKEN_DOT, TOKEN_MINUS, TOKEN_PLUS, TOKEN_STAR, TOKEN_SLASH,
+    TOKEN_COLIN, TOKEN_EQUAL, TOKEN_BANG, TOKEN_LESS, TOKEN_GREATER,
+
+    // two character
+    TOKEN_PLUS_PLUS, TOKEN_MINUS_MINUS,
+    TOKEN_EQUAL_EQUAL, TOKEN_BANG_EQUAL, TOKEN_LESS_EQUAL, TOKEN_GREATER_EQUAL,
+
+    // literals
+    TOKEN_IDENTIFIER, TOKEN_STRING, TOKEN_INTEGER, TOKEN_DECIMAL, TOKEN_ANNOTATION,
+
+    // keywords
+    TOKEN_STRUCT, TOKEN_VAR, TOKEN_FUNCTION,
+
+    TOKEN_ERROR, TOKEN_EOF, TOKEN_SIZE,
 } TokenType;
 
 typedef struct Token {
     char* start;
     i32 length;
+    i32 line;
     TokenType type;
 } Token;
 
-typedef struct LexerState {
+typedef struct Lexer {
+    i32 line;
     char* start;
     char* current;
-} LexerState;
+} Lexer;
 
-void lexer_state_init(LexerState* state, char* code);
-Token lexer_next_token(LexerState* state);
+void lexer_state_init(Lexer* state, char* code);
+Token lexer_next_token(Lexer* state);
 
 #endif
--- src/lexer.c
+++ src/lexer.c
@@ -3,68 +3,216 @@
 #include <ctype.h>
 #include <stdlib.h>
 
-void lexer_state_init(LexerState* state, char* code) {
-    state->start = code;
-    state->current = code;
+void lexer_lexer_init(Lexer* lexer, char* code) {
+    lexer->line = 0;
+    lexer->start = code;
+    lexer->current = code;
 }
 
-static bool is_at_end(LexerState* state) {
-    return *state->current == '\0';
+static bool is_digit(char c) {
+    return c >= '0' && c <= '9';
 }
 
-Token create_token(LexerState* state, TokenType type) {
+static bool is_alpha(char c) {
+    return (c >= 'a' && c <= 'z')
+        || (c >= 'A' && c <= 'Z')
+        || c == '_';
+}
+
+static bool is_at_end(Lexer* lexer) {
+    return *lexer->current == '\0';
+}
+
+static Token create_token(Lexer* lexer, TokenType type) {
     Token token = { 
         .type = type,
-        .start = state->start,
-        .length = state->current - state->start
+        .start = lexer->start,
+        .length = lexer->current - lexer->start,
+        .line = lexer->line,
     };
 
     return token;
 }
 
-char advance(LexerState* state) {
-    state->current++;
-    return state->current[-1];
+static Token error_token(Lexer* lexer, const char* message) {
+    Token token = {
+        .type = TOKEN_ERROR,
+        .start = message,
+        .length = (i32)(strlen(message)),
+        .line = lexer->line,
+    };
+    
+    return token;
 }
 
-char peak(LexerState* state) {
-    return state->current[0];
+static char advance(Lexer* lexer) {
+    lexer->current++;
+    return lexer->current[-1];
 }
 
-Token lexer_next_token(LexerState* state) {
-    // skip whitespace
-    while (isspace(*state->current)) {
-        state->current++;
+static char FORCE_INLINE peak(Lexer* lexer) {
+    return lexer->current[0];
+}
+
+static char peak_next(Lexer* lexer) {
+    if (is_at_end(lexer)) {
+        return '\0';
     }
 
-    state->start = state->current;
+    return lexer->current[1];
+}
 
-    if (is_at_end(state)) {
-        return create_token(state, TokenType_EOF);
+static bool match(Lexer* lexer, char expected) {
+    if (is_at_end(lexer)) {
+        return false;
     }
 
-    char c = advance(state);
+    if (*lexer->current != expected) {
+        return false;
+    }
 
-    if (isalpha(c)) {
-        while (isalnum(peak(state))) {
-            advance(state);
+    lexer->current++;
+    return true;
+}
+
+static bool skip_whitespace(Lexer* lexer) {
+    for (;;) {
+        char c = peak(lexer);
+        switch (c) {
+            case ' ':
+            case '\t':
+            case '\r':
+                advance(lexer);
+                break;
+            case '\n':
+                lexer->line++;
+                advance(lexer);
+                break;
+            default:
+                return;
         }
+    }
+}
 
-        return create_token(state, TokenType_IDENTIFIER);
+static Token string(Lexer* lexer) {
+    while (peek(lexer) != '"' && !is_at_end(lexer)) {
+        if (peek(lexer) == '\n') {
+            lexer->line++;
+        }
+        advance(lexer);
     }
 
-    if (isnumber(c)) {
-        while(isnumber(peak(state))) {
-            advance(state);
-        }
+    if (is_at_end(lexer)) {
+        return error_token(lexer, "Unterminated string.");
+    }
 
-        return create_token(state, TokenType_INTEGER);
+    advance(lexer);
+    return create_token(lexer, TOKEN_STRING);
+}
+
+static Token annotation(Lexer* lexer) {
+    while (is_alpha(peek(lexer)) || is_digit(peek(lexer))) {
+        advance(lexer);
+    }
+
+    return create_token(lexer, TOKEN_ANNOTATION);
+}
+
+static Token number(Lexer* lexer) {
+    while (is_digit(peek(lexer)) || peek(lexer) == '_') {
+        advance(lexer);
+    }
+
+    bool is_decimal = false;
+    if (peek(lexer) == '.' && is_digit(peek_next(lexer))) {
+        is_decimal = true;
+
+        advance(lexer);
+
+        while (is_digit(peek(lexer)) || peek(lexer) == '_') {
+            advance(lexer);
+        }
+    }
+
+    return create_token(lexer, is_decimal ? TOKEN_DECIMAL : TOKEN_INTEGER);
+}
+
+static TokenType check_keyword(Lexer* lexer, i32 start, i32 length,
+        const char* rest, TokenType type) {
+    if (lexer->current - lexer->start == start + length &&
+        memcpy(lexer->start + start, rest, length)== 0) {
+        return type;
+    }
+
+    return TOKEN_IDENTIFIER;
+}
+
+static TokenType identifier_type(Lexer* lexer) {
+    switch (lexer->start[0]) {
+        case 's': return check_keyword(lexer, 1, 5, "truct", TOKEN_STRUCT);
+        case 'v': return check_keyword(lexer, 1, 2, "ar", TOKEN_VAR);
+        case 'f': return check_keyword(lexer, 1, 3, "unc", TOKEN_FUNCTION);
+    }
+
+    return TOKEN_IDENTIFIER;
+}
+
+static Token identifier(Lexer* lexer) {
+    while (is_alpha(peek(lexer)) || is_digit(peek(lexer))) {
+        advance(lexer);
+    }
+
+    return create_token(lexer, idenifier_type(lexer));
+}
+
+Token lexer_next_token(Lexer* lexer) {
+    skip_whitespace(lexer);
+
+    lexer->start = lexer->current;
+
+    if (is_at_end(lexer)) {
+        return create_token(lexer, TOKEN_EOF);
+    }
+
+    char c = advance(lexer);
+
+    if (is_alpha(c)) {
+        return identifier(lexer);
+    }
+
+    if (is_digit(c)) {
+        return number(lexer);
     }
 
     switch (c) {
-        case ':': return create_token(state, TokenType_COLIN);
-        case '=': return create_token(state, TokenType_EQUALS);
+        case '(': return create_token(lexer, TOKEN_PARENTHESIS_OPEN);
+        case ')': return create_token(lexer, TOKEN_PARENTHESIS_CLOSE);
+        case '{': return create_token(lexer, TOKEN_BRACE_OPEN);
+        case '}': return create_token(lexer, TOKEN_BRACE_CLOSE);
+        case '[': return create_token(lexer, TOKEN_BRACKET_OPEN);
+        case ']': return create_token(lexer, TOKEN_BRACKET_CLOSE);
+        case ',': return create_token(lexer, TOKEN_COMMA);
+        case '.': return create_token(lexer, TOKEN_DOT);
+        case '-': return create_token(lexer, TOKEN_MINUS);
+        case '*': return create_token(lexer, TOKEN_STAR);
+        case '/': return create_token(lexer, TOKEN_SLASH);
+        case ':': return create_token(lexer, TOKEN_COLIN);
+        case '+':
+            return create_token(lexer, match(lexer, '+') ? TOKEN_PLUS_PLUS : TOKEN_PLUS);
+        case '=':
+            return create_token(lexer, match(lexer, '=') ? TOKEN_EQUAL_EQUAL : TOKEN_EQUAL);
+        case '!':
+            return create_token(lexer, match(lexer, '=') ? TOKEN_BANG_EQUAL : TOKEN_BANG);
+        case '<':
+            return create_token(lexer, match(lexer, '=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
+        case '>':
+            return create_token(lexer, match(lexer, '=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+        
+        case '"':
+            return string(lexer);
+        case '@':
+            return annotation(lexer);
     }
 
-    return create_token(state, TokenType_INVALID);
+    return error_token(lexer, "Unexpected character.");
 }
