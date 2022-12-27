55e0beb68ec7e0c4645c06f002c4375044f38cd2
Lucrecious <luca.duran@hotmail.com>
2022-12-26 23:28:14 -0500

    Add assignment tests

--- test/declarations_assignments.c
+++ test/declarations_assignments.c
@@ -53,6 +53,11 @@ INTERPRETER_TEST(declaration_i64_explicit,
     "x: i64 = 1; print_expr x;",
     "x (i64) => 1\n")
 
+INTERPRETER_TEST(assignment_i64,
+    "x: i64 = 42; print_expr x; x = 420; print_expr x;",
+    "x (i64) => 42\nx (i64) => 420\n");
+
+
 INTERPRETER_TEST(declaration_i32_default,
     "x: i32; print_expr x;",
     "x (i32) => 0\n")
@@ -65,9 +70,14 @@ INTERPRETER_TEST(declaration_i32_explicit,
     "x: i32 = 42; print_expr x;",
     "x (i32) => 42\n")
 
+INTERPRETER_TEST(assignment_i32,
+    "y := 69; print_expr y; y = 690; print_expr y;",
+    "y (i32) => 69\ny (i32) => 690\n")
+
 INTERPRETER_TEST(declaration_i32_explicit_bool,
     "x: i32 = true; print_expr x;",
     "x (i32) => 1\n")
+
 
 INTERPRETER_TEST(declaration_f64_default,
     "x: f64; print_expr x;",
@@ -81,6 +91,11 @@ INTERPRETER_TEST(declaration_f64_explicit,
     "x: f64 = 420.69; print_expr x;",
     "x (f64) => 420.69\n")
 
+INTERPRETER_TEST(assignment_f64,
+    "x := 420.69; print_expr x; x = 69.42; print_expr x;",
+    "x (f64) => 420.69\nx (f64) => 69.42\n")
+
+
 INTERPRETER_TEST(declaration_symbol_default,
     "x: symbol; print_expr x;",
     "x (symbol) => ''\n")
@@ -92,6 +107,11 @@ INTERPRETER_TEST(declaration_symbol_implicit,
 INTERPRETER_TEST(declaration_symbol_explicit,
     "x: symbol = 'bar'; print_expr x;",
     "x (symbol) => 'bar'\n")
+
+INTERPRETER_TEST(assignment_symbol,
+    "x := 'foo'; print_expr x; x = 'bar'; print_expr x;",
+    "x (symbol) => 'foo'\nx (symbol) => 'bar'\n")
+
 
 INTERPRETER_TEST(declaration_string_default,
     "x: string; print_expr x;",
@@ -105,6 +125,11 @@ INTERPRETER_TEST(declaration_string_explicit,
     "x: string = \"bar\"; print_expr x;",
     "x (string) => bar\n")
 
+INTERPRETER_TEST(assignment_string,
+    "x := \"foo\"; print_expr x; x = \"bar\"; print_expr x;",
+    "x (string) => foo\nx (string) => bar\n")
+
+
 INTERPRETER_TEST(declaration_void_default,
     "x: void; print_expr x;",
     "x (void) => null\n")
@@ -116,6 +141,7 @@ INTERPRETER_TEST(declaration_void_implicit,
 INTERPRETER_TEST(declaration_void_explicit,
     "x: void = null; print_expr x;",
     "x (void) => null\n")
+
 
 INTERPRETER_TEST(declaration_bool_default,
     "x: bool; print_expr x;",
@@ -129,44 +155,78 @@ INTERPRETER_TEST(declaration_bool_explicit,
     "x: bool = false; print_expr x;",
     "x (bool) => false\n")
 
+INTERPRETER_TEST(assignment_bool,
+    "x := false; print_expr x; x = true; print_expr x;",
+    "x (bool) => false\nx (bool) => true\n")
+
+
 INTERPRETER_TEST(declaration_union_explicit_default_void,
     "x: bool | void; print_expr x;",
-    "x (bool|*void) => null\n")
+    "x (bool|void) => null\n")
 
 INTERPRETER_TEST(declaration_union_explicit,
     "x: bool | void = true; print_expr x;",
-    "x (*bool|void) => true\n")
+    "x (bool|void) => true\n")
 
 INTERPRETER_TEST(declaration_union_explicit_no_void,
     "x: bool | string = \"true\"; print_expr x;",
-    "x (bool|*string) => true\n")
+    "x (bool|string) => true\n")
+
+INTERPRETER_TEST(assignment_union_stack_data,
+    "x: bool | i32 = true; print_expr x; x = 32; print_expr x;",
+    "x (bool|i32) => true\nx (bool|i32) => 32\n");
+
+INTERPRETER_TEST(assignment_union_object_data,
+    "x: string | symbol = 'foo'; print_expr x; x = \"bar\"; print_expr x;",
+    "x (string|symbol) => 'foo'\nx (string|symbol) => bar\n");
+
+INTERPRETER_TEST(assignment_union_object_stack_mix_data,
+    "x: bool | symbol = 'foo'; print_expr x; x = false; print_expr x;",
+    "x (bool|symbol) => 'foo'\nx (bool|symbol) => false\n");
+
 
 MU_TEST_SUITE(all_tests) {
     MU_RUN_TEST(declaration_i64_default);
     MU_RUN_TEST(declaration_i64_implicit);
     MU_RUN_TEST(declaration_i64_explicit);
+    MU_RUN_TEST(assignment_i64);
+
     MU_RUN_TEST(declaration_i32_default);
     MU_RUN_TEST(declaration_i32_implicit);
     MU_RUN_TEST(declaration_i32_explicit);
+    MU_RUN_TEST(assignment_i32);
     MU_RUN_TEST(declaration_i32_explicit_bool);
+
     MU_RUN_TEST(declaration_f64_default);
     MU_RUN_TEST(declaration_f64_implicit);
     MU_RUN_TEST(declaration_f64_explicit);
+    MU_RUN_TEST(assignment_f64);
+
     MU_RUN_TEST(declaration_symbol_default);
     MU_RUN_TEST(declaration_symbol_implicit);
     MU_RUN_TEST(declaration_symbol_explicit);
+    MU_RUN_TEST(assignment_symbol);
+
     MU_RUN_TEST(declaration_string_default);
     MU_RUN_TEST(declaration_string_implicit);
     MU_RUN_TEST(declaration_string_explicit);
+    MU_RUN_TEST(assignment_string);
+
     MU_RUN_TEST(declaration_void_default);
     MU_RUN_TEST(declaration_void_implicit);
     MU_RUN_TEST(declaration_void_explicit);
+
     MU_RUN_TEST(declaration_bool_default);
     MU_RUN_TEST(declaration_bool_implicit);
     MU_RUN_TEST(declaration_bool_explicit);
+    MU_RUN_TEST(assignment_bool);
+
     MU_RUN_TEST(declaration_union_explicit_default_void);
     MU_RUN_TEST(declaration_union_explicit);
     MU_RUN_TEST(declaration_union_explicit_no_void);
+    MU_RUN_TEST(assignment_union_stack_data);
+    MU_RUN_TEST(assignment_union_object_data);
+    MU_RUN_TEST(assignment_union_object_stack_mix_data);
 }
 
 int main(int argc, char** argv) {
