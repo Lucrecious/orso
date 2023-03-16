bec00230d41f70bae2fe1941c452fe6242dd5034
Lucrecious <luca.duran@hotmail.com>
2023-03-16 11:44:41 -0400

    Add more control flow tests

--- test/control_flow.c
+++ test/control_flow.c
@@ -32,6 +32,30 @@ INTERPRETER_TEST(if_union_condition_is_null,
 	"x := if true { \"hello world\"; };\nx = null;\nif not x {\n	print x;\n};",
 	"null")
 
+INTERPRETER_TEST(multiple_if_statements,
+    "if true { print 'first'; };\nif true { print 'second'; };",
+    "'first'\n'second'\n")
+
+INTERPRETER_TEST(if_nested_else_if,
+    "if false { print 'first'; } else if false { print 'second'; } else if true { print 'third'; };",
+    "'third'\n")
+
+INTERPRETER_TEST(unless_nested_else_unless,
+    "unless true { print 'first'; } else unless true { print 'second'; } else { print 'third'; };",
+    "'third'\n")
+
+INTERPRETER_TEST(unless_nested_else_if,
+    "unless true { print 'first'; } else if true { print 'second'; } else { print 'third'; };",
+    "'second'\n")
+
+INTERPRETER_TEST(if_else_nested_if,
+    "if false { print 'first'; } else { if true { print 'second'; } else { print 'third'; }; };",
+    "'second'\n")
+
+INTERPRETER_TEST(if_else_nested_unless,
+    "if false { print 'first'; } else { unless false { print 'second'; } else { print 'third'; }; };",
+    "'second'\n")
+
 MU_TEST_SUITE(tests) {
     MU_RUN_TEST(then_branch_print);
     MU_RUN_TEST(else_branch_print);
@@ -41,6 +65,12 @@ MU_TEST_SUITE(tests) {
     MU_RUN_TEST(elseunless_branch_print);
     MU_RUN_TEST(if_union_condition_not_null);
     MU_RUN_TEST(if_union_condition_is_null);
+    MU_RUN_TEST(multiple_if_statements);
+    MU_RUN_TEST(if_nested_else_if);
+    MU_RUN_TEST(unless_nested_else_unless);
+    MU_RUN_TEST(unless_nested_else_if);
+    MU_RUN_TEST(if_else_nested_if);
+    MU_RUN_TEST(if_else_nested_unless);
 }
 
 int main(int argc, char** argv) {
