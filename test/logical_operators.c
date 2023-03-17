723e759f89f7de7e7ab52ea3a0317db5129723c3
Lucrecious <luca.duran@hotmail.com>
2023-03-16 20:45:24 -0400

    Add more logical operation tests

--- test/logical_operators.c
+++ test/logical_operators.c
@@ -52,6 +52,22 @@ INTERPRETER_TEST(and_define,
 	"x := \"\" and null; print_expr x;",
 	"x (string|void) => null\n")
 
+INTERPRETER_TEST(or_shortcircuit_on_block,
+	"x := false or { x := 10; y := 2; x * y; } or false;\nprint x;",
+	"20\n")
+
+INTERPRETER_TEST(and_return_last,
+	"x := true and  { x := 10; y := 2; x * y; } and false;\nprint x;",
+	"false\n")
+
+INTERPRETER_TEST(and_and_ors_infers_full_type,
+	"y := \"\" or null;\ny = \"hello\";\nx := true and y and false;\nprint_expr x;",
+	"x (bool|string|void) => false\n")
+
+INTERPRETER_TEST(bracketed_expressions_narrows_type,
+	"y := \"\" or null;\ny = \"hello\";\nx := true and (y) and false;\nprint_expr x;",
+	"x (bool|string) => false\n")
+
 
 MU_TEST_SUITE(tests) {
     MU_RUN_TEST(not_not_string);
@@ -68,6 +84,10 @@ MU_TEST_SUITE(tests) {
 
     MU_RUN_TEST(or_define);
     MU_RUN_TEST(and_define);
+    MU_RUN_TEST(or_shortcircuit_on_block);
+    MU_RUN_TEST(and_return_last);
+    MU_RUN_TEST(and_and_ors_infers_full_type);
+    MU_RUN_TEST(bracketed_expressions_narrows_type);
 }
 int main(int argc, char** argv) {
     (void)argc; // unused
