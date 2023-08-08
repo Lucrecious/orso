9e04181de141061272a585eb0a9609b36208c618
Lucrecious <luca.duran@hotmail.com>
2023-08-07 23:27:58 -0400

    Add tinycc as submodule

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 
 #include "interpreter.h"
 
+#include <libtcc.h>
+
 #define PROJECT_NAME "orso"
 
 void myerror(OrsoErrorType error, i32 line, const char* message) {
@@ -19,7 +21,49 @@ void mywrite(const char* chars) {
     printf("%s", chars);
 }
 
+typedef int (*int_getter)(void*);
+
 int main(int argc, char **argv) {
+    // if (argc < 2) {
+    //     printf("No script file provided!\n");
+    //     return 1;
+    // }
+
+    // TCCState *s = tcc_new();
+    // if (!s) {
+    //     printf("Could not create tcc state\n");
+    //     return 1;
+    // }
+
+    // tcc_set_output_type(s, TCC_OUTPUT_MEMORY);
+
+    // tcc_add_include_path(s, "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include");
+    // tcc_add_library_path(s, "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/lib");
+
+
+    // if (tcc_add_file(s, argv[1]) == -1) {
+    //     printf("Could not add file: %s\n", argv[1]);
+    //     return 1;
+    // }
+
+    // if (tcc_relocate(s, TCC_RELOCATE_AUTO) < 0) {
+    //     printf("Could not relocate\n");
+    //     return 1;
+    // }
+
+    // void* point = tcc_get_symbol(s, "my_point");
+    // int_getter get_y = (int_getter)tcc_get_symbol(s, "get_y");
+    // if (!point || !get_y) {
+    //     printf("Could not find required symbols\n");
+    //     return 1;
+    // }
+
+    // int y = get_y(point);
+    // printf("y is %d\n", y);
+
+    // tcc_delete(s);
+    // return 0;
+
     if (argc < 2) {
         printf("You must provide a file.");
         exit(1);
