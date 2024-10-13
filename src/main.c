182313159bc2d8f6e598a8a8e29f2ba8bebb51cf
Lucrecious <luca.duran@hotmail.com>
2024-10-13 09:19:08 -0400

    Get hello world to work with some arenas and stuff

--- src/main.c
+++ src/main.c
@@ -4,9 +4,26 @@
 #include "interpreter.h"
 #include "error.h"
 
+#define STRINGT_IMPLEMENTATION
+#include "stringt.h"
+
+#define ARENA_IMPLEMENTATION
+#include "arena.h"
+
 #include <libtcc.h>
 
 #define PROJECT_NAME "orso"
+
+void log_fatal(cstr_t format, ...) {
+    va_list args;
+    va_start(args, format);
+
+    printf("[FATAL] ");
+    vprintf(format, args);
+    printf("\n");
+
+    va_end(args);
+}
 
 void myerror(OrsoError error) {
     switch (error.type) {
@@ -26,37 +43,46 @@ typedef int (*int_getter)(void*);
 
 int main(int argc, char **argv) {
     if (argc < 2) {
-        printf("You must provide a file.");
+        printf("Usage: orso <path/to/file.odl>\n");
         exit(1);
     }
 
-    char* path = realpath(argv[1], NULL);
-    printf("---- %s\n", path);
+    arena_t allocator = {};
+
+    string_t path = cstr2string(argv[1], &allocator);
 
     FILE* file;
-    file = fopen(path, "r");
-    free(path);
+    file = fopen(path.cstr, "r");
 
     if (file == NULL) {
-        printf("Error opening file: %s\n", argv[1]);
+        log_fatal("Unable to open file: %s\n", path.cstr);
         exit(1);
     }
 
-    fseek(file, 0L, SEEK_END);
+    if (fseek(file, 0L, SEEK_END) != 0) {
+        log_fatal("Unable to seek in file: %s\n", path.cstr);
+        exit(1);
+    }
+
     long int size = ftell(file);
     rewind(file);
 
-    char source[size + 1];
+    string_t source;
+    {
+        char source_[size + 1];
 
-    fread(source, size, 1, file);
+        fread(source_, size, 1, file);
 
-    fclose(file);
+        fclose(file);
 
-    source[size] = '\0';
+        source_[size] = '\0';
+
+        source = cstr2string(source_, &allocator);
+    }
 
     OrsoVM vm;
     orso_vm_init(&vm, mywrite, 1000);
-    orso_run_source(&vm, source, myerror);
+    orso_run_source(&vm, source.cstr, myerror);
     orso_vm_free(&vm);
 
     return 0;
